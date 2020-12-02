a8e522aa9ad9844a5912ae7b6cffdbe50b665040
Victor Kuzmanov <victorkuzmanov@gmail.com>
2020-12-02 15:32:34 +0200

    Added ongoing exercise.

---
+++ Technology-Fundamentals-C#-Sept-2020/Excercises/RegexExc/1.Furniture/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string pattern = @">>(\w+)<<(\d+\.?\d*)!(\d+)";
+            Regex regex = new Regex(pattern);
+            double total = 0.0;
+
+            Console.WriteLine("Bought furniture:");
+
+            while ((input = Console.ReadLine())!= "Purchase")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    Console.WriteLine(match.Groups[1].Value);
+                    total += double.Parse(match.Groups[2].Value) * int.Parse(match.Groups[3].Value);
+                }
+            }
+
+            Console.WriteLine($"Total money spend: {total:f2}");
+        }
+    }
+}
