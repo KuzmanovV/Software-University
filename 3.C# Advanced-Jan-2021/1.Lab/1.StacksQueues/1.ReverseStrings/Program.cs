86359f43bce3d3ff31cfbd305db6c43f4d74774d
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-01-01 22:36:16 +0200

    Solved first Advanced problem

---
+++ 3.C# Advanced-Jan-2021/1.Lab/1.StacksQueues/1.ReverseStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<string> stack = new Stack<string>();
+            string output="";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i].ToString());
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output += stack.Pop();
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
