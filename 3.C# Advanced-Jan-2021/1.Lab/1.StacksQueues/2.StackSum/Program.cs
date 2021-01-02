95c1c8cdb10f190b6f3d6df47cb8da5165309127
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-01-02 22:03:37 +0200

    Solved excs added.

---
+++ 3.C# Advanced-Jan-2021/1.Lab/1.StacksQueues/2.StackSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            string command;
+            while ((command = Console.ReadLine().ToUpper()) != "END")
+            {
+                string[] cmd = command.Split();
+
+                switch (cmd[0].ToUpper())
+                {
+                    case "ADD":
+                        stack.Push(int.Parse(cmd[1]));
+                        stack.Push(int.Parse(cmd[2]));
+                        break;
+                    case "REMOVE":
+                        if (int.Parse(cmd[1]) <= stack.Count)
+                        {
+                            for (int i = 1; i <= int.Parse(cmd[1]); i++)
+                            {
+                                stack.Pop();
+                            }
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
