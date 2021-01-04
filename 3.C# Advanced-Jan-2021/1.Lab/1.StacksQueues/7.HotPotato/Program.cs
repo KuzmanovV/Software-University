b6316ed1023fed6f3a8bc122982f1615d8d0eb37
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-01-04 08:55:40 +0200

    Exc solved added.

--- 3.C# Advanced-Jan-2021/1.Lab/1.StacksQueues/7.HotPotato/Program.cs
+++ 3.C# Advanced-Jan-2021/1.Lab/1.StacksQueues/7.HotPotato/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+        }
+    }
+}
