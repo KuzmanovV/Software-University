f4a70d4780f9928ef66787a71b9c7a0a1cfcb49a
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-01-08 23:31:14 +0200

    Solved excs added.

---
+++ 3.C# Advanced-Jan-2021/1.Lab/2.MultidimensionalArrays/6.JaggedArrayModification/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6.JaggedArrayModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows,1];
+            
+            for (int i = 0; i < rows; i++)
+            {
+                    
+            }
+        }
+    }
+}
