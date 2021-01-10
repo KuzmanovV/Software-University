e33caaee2dc6b12399e8c47b4b9305266fcf3348
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-01-10 10:03:57 +0200

    Solved exc added.

---
+++ 3.C# Advanced-Jan-2021/1.Lab/3.SetsAndDictionaries/2.AverageStudentGrade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.AverageStudentGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfStudents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numOfStudents; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (!students.ContainsKey(input[0]))
+                {
+                    //students.Add(input[0], new List<decimal> { decimal.Parse(input[1])});
+                    students[input[0]] = new List<decimal> { decimal.Parse(input[1]) };
+                }
+                else
+                {
+                    students[input[0]].Add(decimal.Parse(input[1]));
+                }
+            }
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Key} -> {string.Join(' ', item.Value)} (avg: {item.Value.Average()})");
+            }
+        }
+    }
+}
---
+++ 3.C# Advanced-Jan-2021/1.Lab/3.SetsAndDictionaries/3.ProductShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
