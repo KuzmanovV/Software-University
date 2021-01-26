4efbbd0a2236f7fc2b79c3a1ff87745603df57c5
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-01-26 09:23:21 +0200

    Solved exc added.

--- 3.C# Advanced-Jan-2021/2.Excercises/2.MultidimensionalArrays/7.KnightGame/Program.cs
+++ 3.C# Advanced-Jan-2021/2.Excercises/2.MultidimensionalArrays/7.KnightGame/Program.cs
@@ -30,8 +30,11 @@ namespace _7.KnightGame
                     }
                 }
 
+                int deleted = 0;
+                int maxKnightRow = -1;
+                int maxKnightCol = -1;
                 int maxAttackCounter = 0;
-                int deletedKnightsCounter = 0;
+
                 do
                 {
                     //Collecting all knights
@@ -45,9 +48,7 @@ namespace _7.KnightGame
                             }
                         }
                     }
-                    
-                    int maxKnightRow = -1;
-                    int maxKnightCol = -1;
+                    maxAttackCounter = 0;
 
                     foreach (int[] knight in knights)
                     {
@@ -55,14 +56,14 @@ namespace _7.KnightGame
                         int knightCol = knight[1];
                         int attackCounter = 0;
 
-                        attackCounter = CountingAttacks(knightRow - 2, knightCol - 1, board, attackCounter);
-                        attackCounter = CountingAttacks(knightRow - 2, knightCol + 1, board, attackCounter);
-                        attackCounter = CountingAttacks(knightRow - 1, knightCol - 2, board, attackCounter);
-                        attackCounter = CountingAttacks(knightRow - 1, knightCol + 2, board, attackCounter);
-                        attackCounter = CountingAttacks(knightRow + 1, knightCol - 2, board, attackCounter);
-                        attackCounter = CountingAttacks(knightRow + 1, knightCol + 2, board, attackCounter);
-                        attackCounter = CountingAttacks(knightRow + 2, knightCol - 1, board, attackCounter);
-                        attackCounter = CountingAttacks(knightRow + 2, knightCol + 1, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow - 2, knightCol - 1, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow - 2, knightCol + 1, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow - 1, knightCol - 2, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow - 1, knightCol + 2, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow + 1, knightCol - 2, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow + 1, knightCol + 2, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow + 2, knightCol - 1, board, attackCounter);
+                        attackCounter += CountingAttacks(knightRow + 2, knightCol + 1, board, attackCounter);
 
                         if (attackCounter > maxAttackCounter)
                         {
@@ -74,25 +75,26 @@ namespace _7.KnightGame
 
                     if (maxAttackCounter != 0)
                     {
-                        board[maxKnightRow, maxKnightCol] = "O";
-                        deletedKnightsCounter++;
+                        board[maxKnightRow, maxKnightCol] = "0";
+                        deleted++;
+                        knights.Clear();
                     }
 
-                } while (maxAttackCounter == 0);
+                } while (maxAttackCounter != 0);
 
-                Console.WriteLine(deletedKnightsCounter);
+                Console.WriteLine(deleted);
             }
         }
 
-        private static int CountingAttacks(int row, int col, string[,] board, int attackCounter)
+        private static int CountingAttacks(int r, int c, string[,] board, int attackCounter)
         {
-            if (row >= 0 && col >= 0 && row < board.GetLongLength(0) && col < board.GetLongLength(1) && board[row, col] == "K")
+            if (r >= 0 && c >= 0 && r < board.GetLongLength(0) && c < board.GetLongLength(1) && board[r, c] == "K")
             {
-                return attackCounter++;
+                return 1;
             }
             else
             {
-                return attackCounter;
+                return 0;
             }
         }
     }
