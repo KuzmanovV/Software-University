12e21c962bdf92a7584f0ba05f43b7ab68c978f4
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-01-29 08:01:32 +0200

    Solved excs added.

---
+++ 3.C# Advanced-Jan-2021/2.Excercises/4.StreamFilesDirectories/4.StreamsFilesDirectories/5.DirectoryTraversal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _5.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> files = new Dictionary<string, Dictionary<string, double>>();
+            string directoryPath = "";
+            string[] fileNames = Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            foreach (string file in fileNames)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string extention = fileInfo.Extension;
+                double kbSize = Math.Round(fileInfo.Length / 1024.0, 3);
+
+                if (!files.ContainsKey(extention))
+                {
+                    files.Add(extention, new Dictionary<string, double>());
+                }
+
+                files[extention].Add(file, kbSize);
+            }
+
+            List<string> output = new List<string>();
+            
+            foreach (var file in files.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                output.Add(file.Key);
+                
+                foreach (var item in file.Value.OrderBy(x => x.Value))
+                {
+                    output.Add($"--{item.Key} - {item.Value}kb");
+                }
+            }
+
+            File.WriteAllLines(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                ,"output.txt"), output);
+        }
+    }
+}
---
+++ 3.C# Advanced-Jan-2021/2.Excercises/4.StreamFilesDirectories/4.StreamsFilesDirectories/6.ZipAndExtract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _6.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ZipArchive zipFile = ZipFile.Open("../../../newzip.zip", ZipArchiveMode.Create);
+            ZipArchiveEntry entry = zipFile.CreateEntryFromFile("../../../copyMe.png", "copyMe.png");
+        }
+    }
+}
