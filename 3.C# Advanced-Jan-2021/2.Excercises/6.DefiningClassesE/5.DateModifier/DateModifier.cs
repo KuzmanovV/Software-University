4e4d7b5dadc494ed33bbf14f9a88169419e9455d
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-02-04 08:44:43 +0200

    Solved

---
+++ 3.C# Advanced-Jan-2021/2.Excercises/6.DefiningClassesE/5.DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5.DateModifier
+{
+    public static class DateModifier
+    {
+public static int GetDiff(string startDateStr, string endDateStr)
+        {
+            DateTime startDate = DateTime.Parse(startDateStr);
+            DateTime endDate = DateTime.Parse(endDateStr);
+
+             TimeSpan diff = startDate-endDate;
+            return Math.Abs(diff.Days);
+        }
+    }
+}
---
+++ 3.C# Advanced-Jan-2021/2.Excercises/6.DefiningClassesE/5.DateModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5.DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            int result = DateModifier.GetDiff(startDate, endDate);
+            Console.WriteLine(result);
+        }
+    }
+}
