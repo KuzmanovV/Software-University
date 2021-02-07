95d4defa0cb04a7bc77b872176d716a96c96f539
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-02-07 09:11:56 +0200

    Solved.

---
+++ 3.C# Advanced-Jan-2021/2.Excercises/8.Generix/3.GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.GenericSwapMethodStrings
+{
+    class Box
+    {
+    }
+}
---
+++ 3.C# Advanced-Jan-2021/2.Excercises/8.Generix/3.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3.GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
