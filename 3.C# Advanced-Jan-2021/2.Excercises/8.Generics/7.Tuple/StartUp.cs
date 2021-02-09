4061ba66f7b82db50fcc53fd12b8e0d9aca69218
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-02-09 09:03:36 +0200

    Solved.

---
+++ 3.C# Advanced-Jan-2021/2.Excercises/8.Generics/7.Tuple/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.Tuple
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            MyTuple<string, string> myTuple = new MyTuple<string, string>(string.Join(" ", input[0], input[1]), input[2]);
+
+            string[] input2 = Console.ReadLine().Split();
+            MyTuple<string, int> myTuple2 = new MyTuple<string, int>(input2[0], int.Parse(input2[1]));
+
+            string[] input3 = Console.ReadLine().Split();
+            MyTuple<int, double> myTuple3 = new MyTuple<int, double>(int.Parse(input3[0]), double.Parse(input3[1]));
+
+            Console.WriteLine(myTuple);
+            Console.WriteLine(myTuple2);
+            Console.WriteLine(myTuple3);
+        }
+    }
+}
