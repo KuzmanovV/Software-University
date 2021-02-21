466e68c037bd04c3fbed37acac78ba37608867f1
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-02-21 10:00:43 +0200

    Solved.

--- 3.C# Advanced-Jan-2021/4.Exams/2.MyAdvanced/1.TheFightForGondor/Program.cs
+++ 3.C# Advanced-Jan-2021/4.Exams/2.MyAdvanced/1.TheFightForGondor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _1.TheFightForGondor
 {
@@ -6,7 +8,82 @@ namespace _1.TheFightForGondor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Input
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<int> plates = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> wave = new Stack<int>();
+
+            int fightingPlate = 0;
+            int wavesCounter = 0;
+            bool platesOver = false;
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                foreach (var item in input)
+                {
+                    wave.Push(item);
+                }
+
+                wavesCounter++;
+                if (wavesCounter % 3 == 0)
+                {
+                    plates.Enqueue(int.Parse(Console.ReadLine()));
+                }
+
+                while (wave.Count > 0)
+                {
+                    if (plates.Count > 0)
+                    {
+                        if (fightingPlate <= 0)
+                        {
+                            fightingPlate = plates.Dequeue();
+                        }
+                        int fight = fightingPlate - wave.Pop();
+
+                        if (fight >= 0)
+                        {
+                            fightingPlate = fight;
+                        }
+                        else
+                        {
+                            wave.Push(0 - fight);
+                        }
+                    }
+                    else
+                    {
+                        platesOver = true;
+                        break;
+                    }
+                }
+
+                if (platesOver)
+                {
+                    break;
+                }
+            }
+
+            //Output
+            if (platesOver)
+            {
+                Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
+                Console.Write($"Orcs left: ");
+                for (int i = 0; i < wave.Count - 1; i++)
+                {
+                    Console.Write($"{wave.Pop()}, ");
+                }
+                Console.WriteLine(wave.Pop());
+            }
+            else
+            {
+                Console.WriteLine("The people successfully repulsed the orc's attack.");
+                Console.Write($"Plates left: ");
+                for (int i = 0; i < plates.Count - 1; i++)
+                {
+                    Console.Write($"{plates.Dequeue()}, ");
+                }
+                Console.WriteLine(plates.Dequeue());
+            }
         }
     }
 }
---
+++ 3.C# Advanced-Jan-2021/4.Exams/2.MyAdvanced/2.Warships/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace _2.Warships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = rows;
+            string[,] matrix = new string[rows, cols];
+
+            string[] input = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            //Input
+            int shipsL = 0;
+            int shipsR = 0;
+            int total = shipsL + shipsR;
+            for (int row = 0; row < rows; row++)
+            {
+                string[] currentRow = Console.ReadLine().Split(" ").ToArray();
+
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+
+                    if (matrix[row, col] == "<")
+                    {
+                        shipsL++;
+                    }
+                    else if (matrix[row, col] == ">")
+                    {
+                        shipsR++;
+                    }
+                }
+            }
+
+            //Main
+            for (int j = 0; j < input.Length; j++)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    string cmd = input[i];
+                    int row = cmd[0];
+                    int col = cmd[1];
+
+                    if (IsPositionValid(row, col, rows, cols))
+                    {
+                    string hit = matrix[row, col];
+                        if (hit == "<")
+                        {
+                            shipsL--;
+                            matrix[row, col] = "X";
+                        }
+                        else if (hit == ">")
+                        {
+                            shipsR--;
+                            matrix[row, col] = "X";
+                        }
+                        else if (hit == "#")
+                        {
+                            //moove left
+                            col--;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                matrix[row, col] = "X";
+                                }
+                            }
+
+                            //moove lu
+                            row--;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                    matrix[row, col] = "X";
+                                }
+                            }
+
+                            //moove r
+                            col++;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                    matrix[row, col] = "X";
+                                }
+                            }
+
+                            //moove r
+                            col++;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                    matrix[row, col] = "X";
+                                }
+                            }
+
+                            //moove d
+                            row++;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                    matrix[row, col] = "X";
+                                }
+                            }
+
+                            //moove d
+                            row++;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                    matrix[row, col] = "X";
+                                }
+                            }
+
+                            //moove l
+                            col--;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                    matrix[row, col] = "X";
+                                }
+                            }
+
+                            //moove l
+                            col--;
+                            if (IsPositionValid(row, col, rows, cols))
+                            {
+                                hit = matrix[row, col];
+                                if (hit == "<")
+                                {
+                                    shipsL--;
+                                    matrix[row, col] = "X";
+                                }
+                                else if (hit == ">")
+                                {
+                                    shipsR--;
+                                    matrix[row, col] = "X";
+                                }
+                                else
+                                {
+                                    matrix[row, col] = "X";
+                                }
+                            }
+                        }
+
+                        if (shipsR<=0 || shipsL<=0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+                    //Output
+                    if (shipsL<=0 && shipsR>0)
+                    {
+                        Console.WriteLine($"Player Two has won the game! {total-shipsR} ships have been sunk in the battle.");
+                    }
+                    else if (shipsR<=0 && shipsL>0)
+                    {
+                        Console.WriteLine($"Player One has won the game! {total - shipsL} ships have been sunk in the battle.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"It's a draw! Player One has {shipsL} ships left. Player Two has {shipsR} ships left.");
+                    }
+        }
+            public static bool IsPositionValid(int row, int col, int rows, int cols)
+            {
+                if (row < 0 || row >= rows)
+                {
+                    return false;
+                }
+                if (col < 0 || col >= cols)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+    }
+}
