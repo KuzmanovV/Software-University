d45046eb33494d23f52359086d053f22cbc56d13
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-03-18 09:42:27 +0200

    Solved.

---
+++ 4.C#OOP/1.Lab/4.Reflection/ReflectionL/1.Stealer/Spy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string AnalyzeAccessModifiers(string classToInvestigate)
+        {
+            Type classType=Type.GetType(classToInvestigate);
+            FieldInfo[] classFields = classType.GetFields(
+                BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+            MethodInfo[] classNonPublicMethods =
+                classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo[] classPublicMethods =
+                classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (FieldInfo field in classFields)
+            {
+                stringBuilder.AppendLine($"{field.Name} must be private!");
+            }
+            foreach (MethodInfo method in classNonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                stringBuilder.AppendLine($"{method.Name} have to be public!");
+            }
+            foreach (MethodInfo method in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                stringBuilder.AppendLine($"{method.Name} have to be private!");
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+
+        public string StealFieldInfo(string classToInvestigate, params string[] fieldsToInvestigate)
+        {
+            Type classType = Type.GetType(classToInvestigate);
+            FieldInfo[] classFields = classType.GetFields(
+                BindingFlags.Instance | BindingFlags.Static |
+                BindingFlags.NonPublic | BindingFlags.Public);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            stringBuilder.AppendLine($"Class under investigation: {classToInvestigate}");
+
+            foreach (FieldInfo field in classFields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                stringBuilder.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
