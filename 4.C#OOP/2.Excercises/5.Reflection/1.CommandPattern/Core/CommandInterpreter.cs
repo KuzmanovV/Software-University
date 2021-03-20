236e609c191f27fe7407a4cd22df1e13f116b292
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-03-20 08:12:46 +0200

    Solved.

---
+++ 4.C#OOP/2.Excercises/5.Reflection/1.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputTokens = args.Split();
+            string commandType = inputTokens[0];
+            string[] commandArguments = inputTokens.Skip(1).ToArray();
+
+            string result = null;
+
+            ICommand command = null;
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x=>x.Name==$"{commandType}Command");
+
+            ICommand instance = (ICommand) Activator.CreateInstance(type);
+
+            result = instance.Execute(commandArguments);
+
+            return result;
+        }
+    }
+}
---
+++ 4.C#OOP/2.Excercises/5.Reflection/1.CommandPattern/Core/Engine.cs
@@ -0,0 +1,24 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+namespace CommandPattern.Core
+{
+    public class Engine: IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                var result = commandInterpreter.Read(input);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
