129bf7c37fc3e11103d890f5d79965ede4157649
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-04-06 07:38:03 +0300

    Solved.

--- 4.C#OOP/3.Exams/Practice/220820Easter/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
+++ 4.C#OOP/3.Exams/Practice/220820Easter/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using TheRace;
 
@@ -5,16 +6,54 @@ namespace TheRace.Tests
 {
     public class RaceEntryTests
     {
-
+        private UnitDriver unitDriver;
+        private UnitCar unitCar;
+        private RaceEntry raceEntry;
         [SetUp]
         public void Setup()
         {
+            unitCar = new UnitCar("Lada",20, 1.5);
+            unitDriver = new UnitDriver("Pe6o", unitCar);
+            raceEntry = new RaceEntry();
+            raceEntry.AddDriver(unitDriver);
         }
 
         [Test]
-        public void TestOne()
+        public void CountCounts()
         {
-            Assert.Pass();
+            Assert.AreEqual(raceEntry.Counter,1);
+        }
+
+        [Test]
+        public void NullDriver()
+        {
+            UnitDriver nullDriver = null;
+
+            Assert.Throws<InvalidOperationException>(() => raceEntry.AddDriver(nullDriver));
+        }
+        
+        [Test]
+        public void lDriverExist()
+        {
+            UnitDriver doubleDriver = new UnitDriver("Pe6o", unitCar);
+
+            Assert.Throws<InvalidOperationException>(() => raceEntry.AddDriver(doubleDriver));
+        }
+        
+        [Test]
+        public void CheckDriverCountMin()
+        {
+            Assert.Throws<InvalidOperationException>(() => raceEntry.CalculateAverageHorsePower());
+        }
+        
+        [Test]
+        public void CalcsAverHorsePower()
+        {
+            UnitCar secondCar = new UnitCar("Ford", 40, 1.5);
+            UnitDriver secondDriver = new UnitDriver("Go6o", secondCar);
+            raceEntry.AddDriver(secondDriver);
+
+            Assert.AreEqual(raceEntry.CalculateAverageHorsePower(),30);
         }
     }
 }
