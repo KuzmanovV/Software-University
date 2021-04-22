7168a60baf3683a7c73b6729045cfef5590e6d7e
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-04-22 08:07:35 +0300

    Solved.

---
+++ JSBasics/2.Excercises/1.First/Deposit.js
@@ -0,0 +1,5 @@
+function calc(i){
+    console.log(Number(i[0])+Number(i[1])*((Number(i[0])*Number(i[2]))/1200));
+}
+
+calc([2350,6,7]);
---
+++ JSBasics/2.Excercises/1.First/RadiansToDegrees.js
@@ -0,0 +1,5 @@
+function convertor(i){
+    console.log(Math.round(i[0]*180/Math.PI));
+}
+
+convertor([3.1416])
