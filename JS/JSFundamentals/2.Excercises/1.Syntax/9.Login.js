e309e4372e4a67aff6bf1af170126d9744e71b9e
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-05-24 11:26:56 +0300

    Solved.

---
+++ JS/JSFundamentals/2.Excercises/1.Syntax/9.Login.js
@@ -0,0 +1,20 @@
+function login(params) {
+    let account = params.shift()
+    let pass = account.split('').reverse().join('')
+
+    for (let i = 0; i < params.length; i++) {
+        if (pass === params[i]) {
+            console.log(`User Acer logged in.`);
+        }
+        else{
+            if (i==3) {
+                console.log(`User ${account} blocked!`);
+                break;
+            }
+
+            console.log(`Incorrect password. Try again.`);
+        }
+    }
+}
+
+login(['momo','omom'])
