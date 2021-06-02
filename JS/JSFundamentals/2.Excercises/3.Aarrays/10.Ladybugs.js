5a09a11424a082c5cb0ef091ec967e64b03f83be
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-06-02 10:47:01 +0300

    Solved.

---
+++ JS/JSFundamentals/2.Excercises/3.Aarrays/10.Ladybugs.js
@@ -0,0 +1,47 @@
+function ladybugs(input) {
+    let field = []
+    let length = input.shift()
+    let indexes = input.shift()
+
+    let indexesArr = indexes.split(' ')
+
+    for (let i = 0; i < length; i++) {
+        field.push(0)
+    }
+
+    for (let i = 0; i < indexesArr.length; i++) {
+       if (indexesArr[i]>=0 && indexesArr[i]<length) {
+        field[indexesArr[i]] = 1
+       }
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        const cmd = input[i].split(' ')
+        let indexToMove = cmd[0]
+        let direction = cmd[1]
+        let mooveLength = cmd[2]
+
+        if (indexToMove >= 0 && indexToMove < length && field[indexToMove] == 1) {
+            field[indexToMove] = 0
+
+            do {
+                if (direction == 'right') {
+                    indexToMove += +mooveLength
+                } else {
+                    indexToMove -= +mooveLength
+                }
+            } while (indexToMove >= 0 && indexToMove < length && field[indexToMove] == 1);
+
+            if (field[indexToMove]===0) {
+                field[indexToMove]=1
+            }
+        }
+    }
+
+    console.log(field.join(' '));
+}
+
+ladybugs([ 5, '3',
+'3 left 2',
+'1 left -2']
+)
