896e680259b5f35642ebbc00db97be0f15406f59
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-06-24 10:26:32 +0300

    Solved.

--- JS/JSFundamentals/2.Excercises/5.ArraysAdvanced/8.ArrayManipulator.js
+++ JS/JSFundamentals/2.Excercises/5.ArraysAdvanced/8.ArrayManipulator.js
@@ -23,7 +23,7 @@ function manipulator(input, cmd) {
                 break;
             case 'sumPairs':
                 let output = []
-                for (let i = 0; i <= input.length; i += 2) {
+                for (let i = 0; i < input.length; i += 2) {
                     if (i + 1 < input.length) {
                         output.push(input[i] + input[i + 1])
                     } else {
@@ -36,6 +36,7 @@ function manipulator(input, cmd) {
                 let forAdd = commandArr.slice(2).map(Number)
                 for (const iterator of forAdd) {
                     input.splice(+commandArr[1], 0, iterator)
+                    commandArr[1]++
                 }
                 break;
             case 'contains':
@@ -46,8 +47,8 @@ function manipulator(input, cmd) {
         }
     }
 
-    return input
+    console.log(`[ ${input.join(', ')} ]`)
 }
 
-console.log(manipulator([1, 2, 3, 4, 5],
-    ['addMany 5 9 8 7 6 5', 'contains 15', 'remove 3', 'shift 1', 'print']));
+manipulator([2, 2, 4, 2, 4],
+    ["add 1 4", "sumPairs", "print"]);
