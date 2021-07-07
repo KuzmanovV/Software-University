b2f8c11ad6df6a270fd49172ac1b8147ae8499a1
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-07-07 11:36:59 +0300

    Solved.

---
+++ JS/JSFundamentals/4.Exams/Heroes.js
@@ -0,0 +1,24 @@
+function solve(input) {
+    
+}
+
+console.log(['2',
+'Solmyr 85 120',
+'Kyrre 99 50',
+'Heal - Solmyr - 10',
+'Recharge - Solmyr - 50',
+'TakeDamage - Kyrre - 66 - Orc',
+'CastSpell - Kyrre - 15 - ViewEarth',
+'End']);
+console.log('----------------');
+console.log(['4',
+    'Adela 90 150',
+    'SirMullich 70 40',
+    'Ivor 1 111',
+    'Tyris 94 61',
+    'Heal - SirMullich - 50',
+    'Recharge - Adela - 100',
+    'CastSpell - Tyris - 1000 - Fireball',
+    'TakeDamage - Tyris - 99 - Fireball',
+    'TakeDamage - Ivor - 3 - Mosquito',
+    'End']);
