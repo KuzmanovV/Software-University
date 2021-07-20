e975b2a9c9e08a396c5519c1f343835fd0e871e7
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-07-20 17:01:53 +0300

    Solved.

---
+++ JS/3.JSAdvacned/1.Lab/3.ObjectsAndComposition/1.CityRecord.js
@@ -0,0 +1,13 @@
+function cr(name, population, treasury) {
+    let city = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+
+    return city
+}
+
+console.log(cr('Tortuga',
+7000,
+15000));
