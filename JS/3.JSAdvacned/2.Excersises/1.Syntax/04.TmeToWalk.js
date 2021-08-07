3c5a5b34b69535a6b86087baf730922d71d45398
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-08-07 10:23:34 +0300

    Solved.

--- JS/3.JSAdvacned/2.Excersises/1.Syntax/04.TmeToWalk.js
+++ JS/3.JSAdvacned/2.Excersises/1.Syntax/04.TmeToWalk.js
@@ -1,13 +1,15 @@
 function ttw(steps, footprint, speed) {
-    let distanceInM = footprint*steps;
-    let rests = Math.floor(distanceInM/500);
-    let time = distanceInM/1000/speed+rests/60;
-    let hours = Math.floor(time/60);
-    time = (time-hours)*60;
-    let minuts = Math.floor(time)
-    let secunds = (time-minuts)*60;
-
-    console.log(`${hours}:${minuts}:${secunds.toFixed(0)}`);
+    let distanceInM = footprint * steps;
+    let rests = Math.floor(distanceInM / 500);
+    let time = distanceInM / 1000 / speed + rests / 60;
+    let hours = Math.floor(time / 60);
+    time = (time - hours) * 60;
+    let minuts = Math.floor(time);
+    let secunds = (time - minuts) * 60;
+    let hoursString = '0' + hours;
+    let minutsString = '0' + minuts;
+    let secundssString = '0' + secunds.toFixed(0);
+    console.log(`${hoursString.slice(-2)}:${minutsString.slice(-2)}:${secundssString.slice(-2)}`);
 }
 
 ttw(4000, 0.60, 5)
