cbd2c23c0c6876ac2a6ad4cba45edb18f6faf180
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-09-26 19:16:44 +0300

    Solved.

--- JS/3.JSAdvacned/2.Excersises/4.DOMIntro/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ JS/3.JSAdvacned/2.Excersises/4.DOMIntro/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('Test');
+    let result = document.getElementById("result");
+    let first = Number(document.getElementById("firstNumber").value);
+    let second = Number(document.getElementById("secondNumber").value);
+    result.textContent = (first-second).toString();
 }
--- JS/3.JSAdvacned/2.Excersises/4.DOMIntro/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ JS/3.JSAdvacned/2.Excersises/4.DOMIntro/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  let textElement = document.getElementById("text").value.toLowerCase().split(' ');
+  let textConvention = document.getElementById('naming-convention').value
+  let result = document.getElementById("result");
+
+  let resultString = '';
+  if (textConvention == 'Camel Case') {
+    resultString=textElement[0];
+    for (let i = 1; i < textElement.length; i++) {
+      resultString += textElement[i][0].toUpperCase() + textElement[i].slice(1);
+    }
+    result.textContent = resultString;
+  } else if (textConvention == 'Pascal Case') {
+    for (let i = 0; i < textElement.length; i++) {
+      resultString += textElement[i][0].toUpperCase() + textElement[i].slice(1);
+    }
+    result.textContent = resultString;
+  } else {
+    result.textContent = 'Error!';
+  }
 }
+
