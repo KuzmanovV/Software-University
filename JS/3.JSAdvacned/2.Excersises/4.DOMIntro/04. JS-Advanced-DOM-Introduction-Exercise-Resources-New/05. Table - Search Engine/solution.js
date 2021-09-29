17db070180534984ec687d6e37d5d1ef79ebbb46
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-09-29 11:52:17 +0300

    Solved.

--- JS/3.JSAdvacned/2.Excersises/4.DOMIntro/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ JS/3.JSAdvacned/2.Excersises/4.DOMIntro/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let fieldE = document.querySelectorAll('tbody tr');
+      let searchE = document.getElementById('searchField').value;
 
+      for (const field of fieldE) {
+         if (field.textContent.includes(searchE)) {
+            field.classList.add('select');
+         } else {
+            field.classList.remove('select');
+         }
+      }
+
+      document.getElementById('searchField').value = "";
    }
 }
