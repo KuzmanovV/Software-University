51f974d1b564b8254019f6065490289a7742aae8
Victor Kuzmanov <victorkuzmanov@gmail.com>
2021-10-07 08:10:19 +0300

    Solved.

--- JS/3.JSAdvacned/2.Excersises/5.DOMManipul.js/04. Fill-Dropdown/dropdown.js
+++ JS/3.JSAdvacned/2.Excersises/5.DOMManipul.js/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+let textE = document.getElementById('newItemText');
+let valueE = document.getElementById('newItemValue');
+
+let option = document.createElement('option');
+option.textContent = textE.value;
+option.value = valueE.value;
+
+document.getElementById('menu').appendChild(option);
+
+textE.value = '';
+valueE.value = '';
 }
--- JS/3.JSAdvacned/2.Excersises/5.DOMManipul.js/05. Encode-and-Decode-Messages/app.js
+++ JS/3.JSAdvacned/2.Excersises/5.DOMManipul.js/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,31 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let texts = document.querySelectorAll('textarea');
+    let buttons = document.querySelectorAll('button');
+
+    let action = {
+        encode: {
+            text: texts[0],
+            btn: buttons[0],
+            func: (char) => String.fromCharCode(char.charCodeAt(0) + 1)
+        },
+        decode: {
+            text: texts[1],
+            btn: buttons[1],
+            func: (char) => String.fromCharCode(char.charCodeAt(0) - 1)
+        }
+    };
+
+    document.getElementById('main').addEventListener('click', (e)=>{
+        if (e.target.tagName!=='BUTTON') {
+            return;
+        }
+
+        let type = e.target.textContent.includes('Encode') ? 'encode' : 'decode';
+
+        let message = action[type].text.value
+                                    .split('').map(action[type].func).join('');
+
+        action.encode.text.value = '';
+        action.decode.text.value = message;
+    })
 }
