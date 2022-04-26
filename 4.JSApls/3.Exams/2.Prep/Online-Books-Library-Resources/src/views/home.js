53243789adf80574b278134033f0bff348aa489a
Victor Kuzmanov <75267166+KuzmanovV@users.noreply.github.com>
2022-04-26 12:47:31 +0300

    Transferred

---
+++ 4.JSApls/3.Exams/2.Prep/Online-Books-Library-Resources/src/views/home.js
@@ -0,0 +1,31 @@
+import { html } from "../lib.js";
+import { getAll } from "../api/data.js";
+
+const homeTemplate = (books) => html`
+<section id="dashboard-page" class="dashboard">
+    <h1>Dashboard</h1>
+
+    ${books.length == 0
+    ? html`<p class="no-books">No books in database!</p>`
+    : html`<ul class="other-books-list">
+        ${books.map(card)}
+    </ul>`}
+</section>`;
+
+const card = (book) => html`
+<li class="otherBooks">
+    <h3>${book.title}</h3>
+    <p>Type: ${book.type}</p>
+    <p class="img"><img src="${book.imageUrl}"></p>
+    <a class="button" href="/details/${book._id}">Details</a>
+</li>`;
+    
+export async function homePage(ctx) {
+    //Guard redirecting
+    // if (getUserData()) {
+    //     return ctx.page.redirect('/memes');
+    // }
+    const books = await getAll();
+
+    ctx.render(homeTemplate(books));
+}
