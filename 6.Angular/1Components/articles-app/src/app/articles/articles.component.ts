fee7b118f3df569f040563f552d2c3a4d105b4d2
Victor Kuzmanov <75267166+KuzmanovV@users.noreply.github.com>
2022-05-31 11:29:49 +0300

    Angular components added

---
+++ 6.Angular/1Components/articles-app/src/app/articles/articles.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticleData } from '../data/data';
+import { Article } from '../models/article.models';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.css']
+})
+export class ArticlesComponent implements OnInit {
+articles: Article[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.articles = new ArticleData().getData();
+  }
+}
---
+++ 6.Angular/1Components/articles-app/src/app/models/article.models.ts
@@ -0,0 +1,12 @@
+import { title } from "process";
+
+export class Article{
+    /**
+     *
+     */
+    constructor(
+        title: string,
+        description: string,
+        author: string,
+        imageUrl: string) {}
+}
