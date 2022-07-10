7a973669953a9dab5c210c988e8eb6d7882f1dfb
Victor Kuzmanov <75267166+KuzmanovV@users.noreply.github.com>
2022-07-10 10:14:02 +0300

    ComponentsWs

---
+++ 8.React/4.ComponentsWS/MyCompsWs/client/src/App.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react';
+
+import * as userService from './services/userService';
+
+import { Header } from "./components/common/Header";
+import { Footer } from "./components/common/Footer";
+import { Search } from "./components/search/Search";
+import { UserList } from "./components/user-list/UserList";
+import "./App.css";
+
+
+function App() {
+  const [users, setUsers] = useState([]);
+  
+  useEffect(()=>{
+userService.getAll()
+.then(users => setUsers(users));
+  }, []);
+
+  console.log(users);
+  
+  return (
+    <div>
+      <Header />
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+          <UserList users={users}/>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
